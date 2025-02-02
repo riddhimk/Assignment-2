4d64124e830fd0613772d61f568385781053045e
Riddhim Kawdia <riddhim10000@gmail.com>
2025-02-02 15:59:42 +0530

    Added the Main.java file

---
+++ Main.java
@@ -0,0 +1,15 @@
+// Name - Riddhim Kawdia
+// Batch - AIML-B2 (2023-27)
+// PRN - 23070126106
+
+class Main {
+	
+	public static void main(String args[]) {
+		
+		ArrayFunctions functions = new ArrayFunctions();
+		functions.evenOdd();
+		functions.smallestDifference();
+		functions.arrayToArrayList();
+		functions.arrayListToArray();
+	}
+}
