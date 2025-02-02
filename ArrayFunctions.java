24c5e1eb21969bdd4bd96bbaa5cafbed1d09b826
Riddhim Kawdia <riddhim10000@gmail.com>
2025-02-02 15:48:19 +0530

    Added evenOdd() method
    Separates the input array elements into even and odd numbers

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -9,3 +9,26 @@ class ArrayFunctions {
 		System.out.println("Array is: "+array);
 		
 	}
+	
+	void evenOdd() {
+		
+		Input in = new Input();
+		int[] array = in.arrayInput();
+		
+		/*Array List allows array creation without declaring size
+		(it is dynamic)*/
+		ArrayList<Integer> even = new ArrayList<Integer>();
+		ArrayList<Integer> odd = new ArrayList<Integer>();
+		
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] % 2 == 0) {
+				even.add(array[i]);
+			}
+			else {
+				odd.add(array[i]);
+			}
+		}
+		display(even);
+		display(odd);
+	}
+	
