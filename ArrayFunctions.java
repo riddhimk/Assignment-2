267e68cd67ecf1149b0dec6fa3f25ad4255a8255
Riddhim Kawdia <riddhim10000@gmail.com>
2025-02-02 15:47:22 +0530

    Added function to display an array

---
+++ ArrayFunctions.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.Math;
+
+class ArrayFunctions {
+	
+	void display(ArrayList<Integer> array) {
+		
+		// while directly printing array in cmd, convert to string
+		System.out.println("Array is: "+array);
+		
+	}
